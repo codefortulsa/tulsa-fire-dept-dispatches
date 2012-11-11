1c90beb201e7b7b94625c55173480de6aaf2615b
John Whitlock <John-Whitlock@ieee.org>
2012-11-11 16:38:55 -0600

    remove old_follow_unit

--- tfdd/dispatches/views.py
+++ tfdd/dispatches/views.py
@@ -22,30 +22,9 @@ def index(request):
         {'dispatches': dispatches}))
 
 def following(request):
-    
     dispatches = Dispatch.objects.order_by('-dispatched')[:10]
     return render_to_response('following.html', RequestContext(request, 
         {'dispatches': dispatches}))
-
-def old_follow_unit(request, unit_id):
-    assert unit_id
-    unit, created = Unit.objects.get_or_create(id=unit_id)
-    if request.method == 'POST':
-        #form = FollowForm(request.POST)
-        #if form.is_valid():
-        #    phone = form.cleaned_data['phone_number']
-        #    follower, created = Follower.objects.get_or_create(
-        #        phone_number=phone)
-        #    follower.units.add(unit)
-            return redirect('dispatches')  # Redirect after POST
-    else:
-        form = FollowForm()  # An unbound form
-
-    c = RequestContext(request, {
-        'form': form, 'unit': unit_id
-    })
-
-    return render_to_response('follow.html', c)
 
 
 def send_text(request):
