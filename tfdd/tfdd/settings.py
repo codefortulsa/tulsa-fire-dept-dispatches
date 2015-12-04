61e82b8d183f923750809953926756285381a50f
groovecoder <luke.crouch@gmail.com>
2015-12-04 17:05:57 -0600

    remove old STATICFILES_DIRS item

--- tfdd/tfdd/settings.py
+++ tfdd/tfdd/settings.py
@@ -221,7 +221,7 @@ STATIC_ROOT = 'staticfiles'
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
-        os.path.join(BASE_DIR, 'static'),
+#        os.path.join(BASE_DIR, 'static'),
 )
 
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
